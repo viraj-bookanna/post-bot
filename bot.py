aa7812af32f29f0a3277dd2bd8bcf570fa284e8f
Indika Viraj <samangoonerathne@gmail.com>
2024-02-11 22:06:05 +0000

    some full change

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
-import os,logging,re
+import os,logging,json
 from telethon import TelegramClient, events
 from telethon.tl.custom.button import Button
 from dotenv import load_dotenv
-from strings import strings,direct_reply
 from pymongo import MongoClient
+from bson import ObjectId
 
 load_dotenv(override=True)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -12,97 +12,170 @@ API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 MONGODB_URL = os.getenv('MONGODB_URL')
-SUPER_ADMINS = [int(i) for i in os.getenv('SUPER_ADMINS', '0').split(',')]
 database = MongoClient(MONGODB_URL).post_bot
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+buttons_post_mgr = [
+    [Button.text('👁‍🗨 Preview', resize=True), Button.text('⚙️ Options', resize=True), Button.text('Get Buttons', resize=True), Button.text('📝 Edit Content', resize=True), ],
+    [Button.text('Cancel', resize=True), Button.text('☑️ Done', resize=True), ],
+]
 
- 
-'''
-==  Non processed handling  ==
-this section contain the direct responses and access checking part
-see strings.py for default string values
-'''
+def urlfy(data):
+    if data==[]:
+        return None
+    elif type(data)==list:
+        kbd = []
+        for item in data:
+            kbd.append(urlfy(item))
+        return kbd
+    elif type(data)==dict:
+        return Button.url(data['text'], data['url'])
+def inlinefy(data, prefix, level=0):
+    if type(data)==list:
+        kbd = []
+        for i in range(len(data)):
+            kbd.append(inlinefy(data[i], f'{prefix}-{i}', level+1))
+        if level==0:
+            kbd.append([Button.inline('➕', f'{prefix}-+')])
+        else:
+            kbd.append(Button.inline('➕', f'{prefix}-+'))
+        return kbd
+    elif type(data)==dict:
+        return Button.inline(data['text'], prefix)
+
 @bot.on(events.NewMessage(func=lambda e:e.is_private))
 async def handler(event):
-    if event.chat_id not in SUPER_ADMINS and database.admins.find_one({'chat_id': event.chat_id}) is None:
-        await event.respond(strings['no_access'])
-    elif event.message.text in direct_reply.keys():
-        await event.respond(direct_reply[event.message.text])
-    else:
-        return
-    raise events.StopPropagation
-
-'''
-==  Processed handling  ==
-'''
-# === COMMAND HANDLING (SUPER ADMINS ONLY) ===
-@bot.on(events.NewMessage(func=lambda e:e.is_private))
-async def handler(event):
-    if event.chat_id not in SUPER_ADMINS and database.admins.find_one({'chat_id': event.chat_id}) is None:
-        await event.respond(strings['no_access'])
-        return
-    command = event.message.text.split(' ')
-    if command[0]=='/add_admin':
-        if database.admins.find_one({'chat_id': int(command[1])}) is not None:
-            await event.respond(strings['admin_exists'])
-        elif len(command)==3:
-            database.admins.insert_one({'chat_id': int(command[1]),'nick': command[2]})
-            await event.respond(strings['new_admin_added'])
-        else:
-            await event.respond(strings['invalid_syntax'])
-    
-    elif command[0]=='/remove_admin':
-        if database.admins.find_one({'chat_id': int(command[1])}) is not None:
-            database.admins.delete_one({'chat_id': int(command[1])})
-            await event.respond(strings['admin_removed'])
-        else:
-            await event.respond(strings['admin_404'])
-    
-    elif command[0]=='/admins':
-        admin_list = [f"{a['nick']}: `{a['chat_id']}`" for a in database.admins.find()]
-        await event.respond(strings['admin_list_title']+"\n".join(admin_list))
-    
-    elif command[0]=='/add_chat':
-        if database.my_chats.find_one({'chat_id': command[1]}) is not None:
-            await event.respond(strings['chat_already_added'])
-        elif len(command)==3:
-            database.my_chats.insert_one({'chat_id': int(command[1]),'nick': command[2]})
-            await event.respond(strings['new_chat_added'])
-        else:
-            await event.respond(strings['invalid_syntax'])
-
-    elif command[0]=='/remove_chat':
-        if database.my_chats.find_one({'chat_id': int(command[1])}) is not None:
-            database.my_chats.delete_one({'chat_id': int(command[1])})
-            database.chat_posts.delete_many({'chat_id': int(command[1])})
-            await event.respond(strings['chat_removed'])
-        else:
-            await event.respond(strings['chat_404'])
-    
-    elif command[0]=='/chats':
-        chat_list = [f"{a['nick']}: `{a['chat_id']}`" for a in database.my_chats.find()]
-        await event.respond(strings['chat_list_title']+"\n".join(chat_list))
-
-    else:
-        return
-    raise events.StopPropagation
-
-# === COMMAND HANDLING (ALL) ===
-@bot.on(events.NewMessage(func=lambda e:e.is_private))
-async def handler(event):
-    m = re.search(r"^https?://t\.me/c/(\d+)/(\d+)$", event.message.text)
-    if m:
-        chatId = int('-100'+m[1])
-        messageId = int(m[2])
-        post = database.chat_posts.find_one({
-            'chat_id': chatId,
-            'message_id': messageId
+    user = database.users.find_one({'chat_id': event.chat_id})
+    if user is None:
+        sender = await event.get_sender()
+        database.users.insert_one({
+            'chat_id': sender.id,
+            'first_name': sender.first_name,
+            'last_name': sender.last_name
         })
-        if post is None:
-            await event.respond(strings['post_404'])
+    if event.message.text == '/start':
+        buttons = [
+            [Button.text('📌 Create post', resize=True), Button.text('📝Edit Post', resize=True), ]
+        ]
+        await event.respond('''Welcome 🌺
+using this bot you can create cool posts with buttons, markdown text, HTML text, and embedded links.
+then you can send it anywhere you want using inline mode
+if you want to send the post to your channel without inline mode you need to get your own instance of this bot.''', buttons=buttons)
+    
+    elif event.message.text == '/help':
+        await event.respond('press /start', buttons=Button.clear())
+    
+    elif event.message.text == '📌 Create post':
+        user['next'] = 'create_post'
+        user['post_id'] = None
+        database.users.update_one({'_id': user['_id']}, {"$set": user})
+        await event.respond("Send your post's content it can be anything. (text, 🖼photo, 🎙audio, ...)", buttons=Button.clear())
+    
+    elif event.message.text == '📝Edit Post':
+        user['next'] = 'edit_post'
+        database.users.update_one({'_id': user['_id']}, {"$set": user})
+        await event.respond("Enter the post number that you want to edit. Ex: 1", buttons=Button.clear())
+
+    elif user['next'] == 'create_post':
+        if user.get('post_id', None) is not None:
+            data = database.posts.find_one({'_id': ObjectId(user['post_id'])})
         else:
-            buttons = [Button.inline(strings['add_btn'], f'+btn.{chatId}.{messageId}')]
-            await event.respond(post['text'], buttons=buttons)
+            data = {'chat_id': event.chat_id}
+        print('1 -->', data)
+        if event.message.media is not None:
+            data['media'] = event.message.id
+            media = event.message.media
+        elif 'media' in data:
+            media = (await bot.get_messages(event.chat_id, ids=data['media'])).media
+        else:
+            media = None
+        if event.message.text!='' or event.message.text is not None:
+            data['text'] = event.message.text
+        if user.get('post_id', None) is not None:
+            database.posts.update_one({'_id': data['_id']}, {"$set": data})
+        else:
+            user['post_id'] = database.posts.insert_one(data).inserted_id
+            database.users.update_one({'_id': user['_id']}, {"$set": user})
+        print('2 -->', data)
+        buttons = inlinefy([] if 'buttons' not in data else data['buttons'], user['post_id'])
+        msg = await event.respond(data['text'], file=media, buttons=buttons)
+        await msg.reply('''This is a preview of how your message would look like.
+you can edit parse_mode and webpage preview by clicking on Options
+to add inline buttons, click on the ➕ key on the row you want to add a button.
+to delete inline buttons click on the button you want to remove.
+to preview the message (without the extra ➕ buttons) click on preview.
+when you finished editing, press Done to receive the `post number`.''', buttons=buttons_post_mgr)
+
+    elif user['next'] == 'add_btn_text':
+        user['btn_data']['text'] = event.message.text
+        user['next'] = 'add_btn_url'
+        database.users.update_one({'_id': user['_id']}, {"$set": user})
+        await event.respond("Please send url for button:", buttons=Button.clear())
+    
+    elif user['next'] == 'add_btn_url':
+        user['next'] = None
+        database.users.update_one({'_id': user['_id']}, {"$set": user})
+        data = database.posts.find_one({'_id': ObjectId(user['post_id'])})
+        b_list = [] if 'buttons' not in data else data['buttons']
+        if len(user['btn_data']['address'])==1:
+            b_list.append([{'text': user['btn_data']['text'], 'url': event.message.text}])
+        else:
+            b_list[int(user['btn_data']['address'][0])].append({'text': user['btn_data']['text'], 'url': event.message.text})
+        data['buttons'] = b_list
+        database.posts.update_one({'_id': data['_id']}, {"$set": data})
+        if 'media' in data:
+            media = (await bot.get_messages(event.chat_id, ids=data['media'])).media
+        else:
+            media = None
+        msg = await event.respond(data['text'],  file=media, buttons=inlinefy(b_list, user['post_id']))
+        await msg.reply('''This is a preview of how your message would look like.
+you can edit parse_mode and webpage preview by clicking on Options
+to add inline buttons, click on the ➕ key on the row you want to add a button.
+to delete inline buttons click on the button you want to remove.
+to preview the message (without the extra ➕ buttons) click on preview.
+when you finished editing, press Done to receive the `post number`.''', buttons=buttons_post_mgr)
+    
+    elif event.message.text in ['👁‍🗨 Preview','⚙️ Options','Get Buttons','📝 Edit Content','Cancel','☑️ Done'] and user.get('post_id', None) is not None:
+        data = database.posts.find_one({'_id': ObjectId(user['post_id'])})
+        b_list = [] if 'buttons' not in data else data['buttons']
+        if event.message.text == '👁‍🗨 Preview':
+            if 'media' in data:
+                media = (await bot.get_messages(event.chat_id, ids=data['media'])).media
+            else:
+                media = None
+            await event.respond(data['text'],  file=media, buttons=urlfy(b_list))
+        elif event.message.text == '⚙️ Options':
+            pass
+        elif event.message.text == 'Get Buttons':
+            await event.respond(json.dumps(b_list, indent=4))
+        elif event.message.text == '📝 Edit Content':
+            pass
+        elif event.message.text == 'Cancel':
+            pass
+        elif event.message.text == '☑️ Done':
+            pass
+
+@bot.on(events.CallbackQuery(func=lambda e:e.is_private))
+async def handler(event):
+    user = database.users.find_one({'chat_id': event.chat_id})
+    if user is None:
+        sender = await event.get_sender()
+        database.users.insert_one({
+            'chat_id': sender.id,
+            'first_name': sender.first_name,
+            'last_name': sender.last_name
+        })
+    query = event.data.decode().split('-')
+    print(query)
+    data = database.posts.find_one({'_id': ObjectId(query[0])})
+    b_list = [] if 'buttons' not in data else data['buttons']
+    if query[-1] == '+':
+        user['post_id'] = query[0]
+        user['btn_data'] = {
+            'address': query[1:]
+        }
+        user['next'] = 'add_btn_text'
+        database.users.update_one({'_id': user['_id']}, {"$set": user})
+        await event.respond('Please send text for button:', buttons=Button.clear())
 
 with bot:
     bot.run_until_disconnected()
